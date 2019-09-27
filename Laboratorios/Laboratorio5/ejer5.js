49d83b091b6b9ed2e7518234d30e9e269f61ca4e
erikalilianar22 <00106416@uca.edu.sv>
2019-09-27 10:39:25 -0600

    ejercicios

---
+++ Laboratorios/Laboratorio5/ejer5.js
@@ -0,0 +1,7 @@
+/*Palíndromo: Se dice que una palabra es palíndroma cuando se puede leer de la misma manera tanto de derecha a izquierda y viceversa. Crear una función en JavaScript que reciba como parámetro una palabra. La función debe retornar si la palabra es palíndroma o no. */
+
+function palindromo(palabra){
+    let palabra="";
+
+    palabra.split("").reverse();
+}
