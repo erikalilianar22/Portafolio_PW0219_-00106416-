a4b90113369f0a465e96af18ea8872536e20acab
erikalilianar22 <00106416@uca.edu.sv>
2019-10-06 17:55:23 -0600

    ejercicio

---
+++ Laboratorios/Laboratorio6/main.js
@@ -0,0 +1,18 @@
+let counter = 0;
+
+let counter_btn = document.querySelector("#counter-btn");
+let counter_space = document.querySelector("#counter-space");
+
+let add = (function() {
+  let counter = 0;
+
+  return function() {
+    counter += 1;
+    counter_space.innerHTML = counter;
+    return counter;
+  };
+})();
+
+document.getElementById("counter-btn").addEventListener("click", function() {
+  add();
+});
